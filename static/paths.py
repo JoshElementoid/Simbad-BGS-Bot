cf6105fcaeff01b0b625c8f46c6984db363da0d9
JoshElementoid <79376133+JoshElementoid@users.noreply.github.com>
2021-05-03 01:36:22 -0400

    Add files via upload

---
+++ static/paths.py
@@ -0,0 +1,2 @@
+simbad_controlled_full_path = "C:\\Users\\Josh\\Desktop\\Misc\\Bot2\\static\\controlled_full.json"
+simbad_controlled_influence_path = "C:\\Users\\Josh\\Desktop\\Misc\\Bot2\\static\\influence.json"
